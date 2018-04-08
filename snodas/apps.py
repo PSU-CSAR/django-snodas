e4e72073379729474351c2108b4489d0cdbecd20
jkeifer <jarrettkeifer@gmail.com>
2018-04-08 13:43:59 -0700

    more cleanup

--- snodas/apps.py
+++ snodas/apps.py
@@ -1,11 +1,7 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
-from django.db.utils import Error
 
 
 class SnodasConfig(AppConfig):
     name = 'snodas'
     verbose_name = _("SNODAS")
-
-    def ready(self):
-        import snodas.signals
--- snodas/signals.py
+++
@@ -1,2 +0,0 @@
-from django.db.models.signals import pre_delete
-from django.dispatch import receiver
