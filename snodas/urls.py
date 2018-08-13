88b055431066093d2475d31aee9d5dd2ed4d6c0e
Jarrett Keifer <jarrettkeifer@gmail.com>
2018-08-12 22:02:57 -0700

    fix broken urls imports

--- snodas/urls.py
+++ snodas/urls.py
@@ -2,15 +2,15 @@ from django.conf.urls import include, url
 
 from .settings import REST_ROOT, DEBUG, INSTALLED_APPS
 
-from . import views
+from .views import snodas_tiles, pourpoint_tiles, snodas_stats
 
 
 rest_urls = [
-    url(r"^tiles/$", views.snodas.list_dates),
-    url(r"^tiles/(?P<date>\d{4}-\d{2}-\d{2})/(?P<zoom>\d{1,2})/(?P<x>\d+)/(?P<y>\d+).(?P<format>png|jpg|jpeg)$", views.snodas.get_tile),
-    url(r"^pourpoints/(?P<zoom>\d{1,2})/(?P<x>\d+)/(?P<y>\d+).mvt$", views.pourpoints.get_tile),
-    url(r"^query/(?P<start_year>\d{4})/(?P<end_year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$", views.snodas_stats.get_for_date),
-    url(r"^query/(?P<start_year>\d{4})/(?P<end_year>\d{4})/(?P<doy>\d{1-3})/$", views.snodas_stats.get_for_doy),
+    url(r"^tiles/$", snodas_tiles.list_dates),
+    url(r"^tiles/(?P<date>\d{4}-\d{2}-\d{2})/(?P<zoom>\d{1,2})/(?P<x>\d+)/(?P<y>\d+).(?P<format>png|jpg|jpeg)$", snodas_tiles.get_tile),
+    url(r"^pourpoints/(?P<zoom>\d{1,2})/(?P<x>\d+)/(?P<y>\d+).mvt$", pourpoint_tiles.get_tile),
+    url(r"^query/(?P<start_year>\d{4})/(?P<end_year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$", snodas_stats.get_for_date),
+    url(r"^query/(?P<start_year>\d{4})/(?P<end_year>\d{4})/(?P<doy>\d{1-3})/$", snodas_stats.get_for_doy),
 ]
 
 # standard django url patterns
