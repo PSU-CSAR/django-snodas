f75b76ac10acdd010687747a69f8d8214af71dfd
Jarrett Keifer <jkeifer@pdx.edu>
2024-04-21 17:26:06 -0700

    refine API: switch to query params, add links, add root

--- snodas/urls.py
+++ snodas/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.http import HttpRequest, HttpResponse
-from django.urls import path
+from django.urls import path, reverse
 
 from ninja import NinjaAPI
 
@@ -13,6 +13,57 @@ from snodas.views import (
 
 
 api = NinjaAPI()
+
+
+# API root
+@api.get(
+    '/',
+    response=types.LandingPage,
+    exclude_none=True,
+)
+def api_root(request: HttpRequest) -> types.LandingPage:
+    return types.LandingPage(
+        title=(
+            'Portland State University (PSU) Center for Spatial '
+            'Analysis and Research (CSAR) SNODAS tools API'
+        ),
+        description=(
+            'API providing pourpoint/AOI metadata and SNODAS statistics'
+        ),
+        links=[
+            types.Link(
+                href=request.build_absolute_uri(
+                    reverse(f'{api.urls_namespace}:api_root'),
+                ),
+                title='API Root',
+                type='application/json',
+                rel='self',
+            ),
+            types.Link(
+                href=request.build_absolute_uri(
+                    reverse(f'{api.urls_namespace}:openapi-json'),
+                ),
+                title='OpenAPI JSON Spec',
+                type='application/json',
+                rel='service-desc',
+            ),
+            types.Link(
+                href=request.build_absolute_uri(
+                    reverse(f'{api.urls_namespace}:openapi-view'),
+                ),
+                title='Interactive API Documentation',
+                type='text/html',
+                rel='service-doc',
+            ),
+            types.Link(
+                href=request.build_absolute_uri(
+                    reverse(f'{api.urls_namespace}:get_pourpoints'),
+                ),
+                title='List all pourpoints',
+                type='application/geo+json',
+            ),
+        ],
+    )
 
 
 # Tile routes
@@ -36,13 +87,15 @@ api.get(
 @api.get(
     '/pourpoints/',
     response=types.PourPoints,
+    exclude_none=True,
 )
 def get_pourpoints(
     request: HttpRequest,
     response: HttpResponse,
 ):
     response['Content-Type'] = 'application/geo+json'
-    return pourpoints.get_points()
+    return pourpoints.get_points().build_links(request, api)
+
 
 @api.get(
     '/pourpoints/{zoom}/{x}/{y}.mvt',
@@ -60,10 +113,10 @@ def get_pourpoint_tile(
     )
 
 
-# by ID
 @api.get(
     '/pourpoints/{pourpoint_id}/',
     response=types.PourPoint,
+    exclude_none=True,
 )
 def get_pourpoint_by_id(
     request: HttpRequest,
@@ -71,67 +124,13 @@ def get_pourpoint_by_id(
     response: HttpResponse,
 ):
     response['Content-Type'] = 'application/geo+json'
-    return pourpoints.get_point(pourpoint_id)
+    return pourpoints.get_point(pourpoint_id).build_links(request, api, full=True)
 
 
-@api.get(
-    '/pourpoints/{pourpoint_id}/stat/{start_date}/{end_date}/',
-    response=types.PourPointStats,
-)
-def id_stat_range_query(
-    request: HttpRequest,
-    pourpoint_id: int,
-    start_date: types.Date,
-    end_date: types.Date,
-) -> types.PourPointStats:
-    pourpoint = pourpoints.get_point(pourpoint_id)
-    query = types.DateRangeQuery(
-        start_date=start_date,
-        end_date=end_date,
-    )
-    return types.PourPointStats(
-        pourpoint=pourpoint,
-        query=query,
-        results=stats.get_pourpoint_stats(
-            pourpoint.id,
-            query,
-        ),
-    )
-
-
-@api.get(
-    '/pourpoints/{pourpoint_id}/stat/{month}/{day}/{start_year}/{end_year}/',
-    response=types.PourPointStats,
-)
-def id_stat_doy_query(
-    request: HttpRequest,
-    pourpoint_id: int,
-    month: types.Month,
-    day: types.Day,
-    start_year: types.Year,
-    end_year: types.Year,
-):
-    pourpoint = pourpoints.get_point(pourpoint_id)
-    query = types.DOYQuery(
-        month=month,
-        day=day,
-        start_year=start_year,
-        end_year=end_year,
-    )
-    return types.PourPointStats(
-        pourpoint=pourpoint,
-        query=query,
-        results=stats.get_pourpoint_stats(
-            pourpoint.id,
-            query,
-        ),
-    )
-
-
-# by triplet
 @api.get(
     '/pourpoints/by-triplet/{station_triplet}/',
     response=types.PourPoint,
+    exclude_none=True,
 )
 def get_pourpoint_by_triplet(
     request: HttpRequest,
@@ -139,20 +138,24 @@ def get_pourpoint_by_triplet(
     response: HttpResponse,
 ):
     response['Content-Type'] = 'application/geo+json'
-    return pourpoints.get_point(station_triplet)
+    return pourpoints.get_point(station_triplet).build_links(request, api, full=True, from_triplet=True)
 
 
 @api.get(
-    '/pourpoints/by-triplet/{station_triplet}/stat/{start_date}/{end_date}/',
+    '/pourpoints/{pourpoint_id}/stats/date-range', #/{start_date}/{end_date}/',
     response=types.PourPointStats,
+    exclude_none=True,
 )
-def triplet_stat_range_query(
+def id_stat_range_query(
     request: HttpRequest,
-    station_triplet: types.StationTriplet,
+    pourpoint_id: int,
     start_date: types.Date,
     end_date: types.Date,
 ) -> types.PourPointStats:
-    pourpoint = pourpoints.get_point(station_triplet)
+    pourpoint = pourpoints.get_point(pourpoint_id).build_links(
+        request,
+        api,
+    )
     query = types.DateRangeQuery(
         start_date=start_date,
         end_date=end_date,
@@ -164,22 +167,26 @@ def triplet_stat_range_query(
             pourpoint.id,
             query,
         ),
-    )
+    ).build_links(request, api)
 
 
 @api.get(
-    '/pourpoints/by-triplet/{station_triplet}/stat/{month}/{day}/{start_year}/{end_year}/',
+    '/pourpoints/{pourpoint_id}/stats/doy', #/{month}/{day}/{start_year}/{end_year}/',
     response=types.PourPointStats,
+    exclude_none=True,
 )
-def triplet_stat_doy_query(
+def id_stat_doy_query(
     request: HttpRequest,
-    station_triplet: types.StationTriplet,
+    pourpoint_id: int,
     month: types.Month,
     day: types.Day,
-    start_year: types.Year,
-    end_year: types.Year,
-) -> types.PourPointStats:
-    pourpoint = pourpoints.get_point(station_triplet)
+    start_year: types.Year = 2004,
+    end_year: types.Year = 9999,
+):
+    pourpoint = pourpoints.get_point(pourpoint_id).build_links(
+        request,
+        api,
+    )
     query = types.DOYQuery(
         month=month,
         day=day,
@@ -193,7 +200,7 @@ def triplet_stat_doy_query(
             pourpoint.id,
             query,
         ),
-    )
+    ).build_links(request, api)
 
 
 # legacy query endpoints for UI
